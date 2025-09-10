1069e02cade24dbbcbf876d2a6a712f40c829166
Shaqibul-Neil <shaqibul.islam.neil@gmail.com>
2025-09-11 00:20:48 +0600

    get set primitive

--- script.js
+++ script.js
@@ -3,3 +3,19 @@
 // localStorage.getItem()
 // localStorage.removeItem()
 // localStorage.clear()
+
+//How to set and get objects to local storage
+const addNumberToLS = () => {
+  const num = Math.ceil(Math.random() * 100);
+  console.log(num);
+  localStorage.setItem("number", num);
+};
+
+document.querySelector(".addBtn").addEventListener("click", addNumberToLS);
+
+const getNumberFromLS = () => {
+  const num = localStorage.getItem("number");
+  console.log("Get from local Storage", num);
+};
+
+document.querySelector(".getBtn").addEventListener("click", getNumberFromLS);
